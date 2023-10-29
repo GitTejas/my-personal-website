6724d4c84588fee6b559e4adb3da20ce57790a72
Tejas Nampalli <TSNampalli@gmail.com>
2023-10-29 18:00:31 -0400

    Added paragraphs

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "Thank you for the inspiration!";
 
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("DOMContentLoaded", () => console.log("we are up!"));
+
