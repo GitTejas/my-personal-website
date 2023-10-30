d4215533cb0083c1d5b1e0023e6276e5c82ad6a7
Tejas Nampalli <TSNampalli@gmail.com>
2023-10-29 20:08:19 -0400

    added eventlistener

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.querySelector("body").appendChild(h2);
 
 document.addEventListener("DOMContentLoaded", () => console.log("we are up!"));
 
+
+const bPress = document.getElementById('wordz');
+function clickAlert() {
+  alert('YOU SHOWED LUV!');
+}
+bPress.onclick('click', clickAlert);
